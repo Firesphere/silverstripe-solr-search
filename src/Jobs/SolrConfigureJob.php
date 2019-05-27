fd4bd65a20bee84389957f752a5d418bf2464a6d
Simon Erkelens <github@casa-laguna.net>
2019-05-27 19:16:39 +1200

    Add a configuration job
    - Add a Queued Job
    - Minor fixups on php docblocks

---
+++ src/Jobs/SolrConfigureJob.php
@@ -0,0 +1,35 @@
+<?php
+
+
+namespace Firesphere\SolrSearch\Jobs;
+
+
+use Firesphere\SolrSearch\Tasks\SolrConfigureTask;
+use SilverStripe\Control\NullHTTPRequest;
+use SilverStripe\Core\Injector\Injector;
+use Symbiote\QueuedJobs\Services\AbstractQueuedJob;
+
+class SolrConfigureJob extends AbstractQueuedJob
+{
+
+    /**
+     * @return string
+     */
+    public function getTitle()
+    {
+        return 'Configure new or re-configure existing Solr cores';
+    }
+
+    /**
+     * Do some processing yourself!
+     * @throws \ReflectionException
+     */
+    public function process()
+    {
+        /** @var SolrConfigureTask $task */
+        $task = Injector::inst()->get(SolrConfigureTask::class);
+        $task->run(new NullHTTPRequest());
+
+        $this->isComplete = true;
+    }
+}
