11d9ef412d437796c8fe6165c422f803a8c0ffc0
Marco Hermo <marcz@lab1521.com>
2019-06-10 08:25:00 +1200

    CircleCI: Permission changes

--- tests/unit/SolrConfigureJobTest.php
+++ tests/unit/SolrConfigureJobTest.php
@@ -27,14 +27,12 @@ class SolrConfigureJobTest extends SapphireTest
         $this->assertEquals('Configure new or re-configure existing Solr cores', $this->job->getTitle());
     }
 
-    // @todo fix the permission issues etc.
-//    public function testProcess()
-//    {
-//        $this->job->process();
-//
-//        $solrResponse = file_get_contents('http://localhost:8983/solr/TestIndex/admin/ping');
-//        $response = json_decode($solrResponse);
-//        $this->assertEquals('OK', $response->status);
-//        $this->assertEquals('10', $response->params->rows);
-//    }
+    public function testProcess()
+    {
+        $this->job->process();
+        $solrResponse = file_get_contents('http://localhost:8983/solr/TestIndex/admin/ping');
+        $response = json_decode($solrResponse);
+        $this->assertEquals('OK', $response->status);
+        $this->assertEquals('10', $response->responseHeader->params->rows);
+    }
 }
