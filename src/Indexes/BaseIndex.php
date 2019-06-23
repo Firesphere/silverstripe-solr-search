0c7d9e070606c7608d4e684b974fdf11ef90ad76
Simon Erkelens <github@casa-laguna.net>
2019-06-24 00:15:11 +1200

    Move getters and setters to a trait
    - This should improve code quality

--- src/Indexes/BaseIndex.php
+++ src/Indexes/BaseIndex.php
@@ -9,6 +9,7 @@ use Firesphere\SolrSearch\Queries\BaseQuery;
 use Firesphere\SolrSearch\Results\SearchResult;
 use Firesphere\SolrSearch\Services\SchemaService;
 use Firesphere\SolrSearch\Services\SolrCoreService;
+use Firesphere\SolrSearch\Traits\BaseIndexTrait;
 use Firesphere\SolrSearch\Traits\GetterSetterTrait;
 use LogicException;
 use Minimalcode\Search\Criteria;
@@ -35,6 +36,7 @@ abstract class BaseIndex
     use Extensible;
     use Configurable;
     use GetterSetterTrait;
+    use BaseIndexTrait;
 
     private static $fieldTypes = [
         'FulltextFields',
@@ -471,188 +473,5 @@ abstract class BaseIndex
                 $this->getFilterFields()
             )
         );
-    }
-
-    /**
-     * @return array
-     */
-    public function getFulltextFields(): array
-    {
-        return $this->fulltextFields;
-    }
-
-    /**
-     * @param array $fulltextFields
-     * @return $this
-     */
-    public function setFulltextFields($fulltextFields): self
-    {
-        $this->fulltextFields = $fulltextFields;
-
-        return $this;
-    }
-
-    /**
-     * @return array
-     */
-    public function getSortFields(): array
-    {
-        return $this->sortFields;
-    }
-
-    /**
-     * @param array $sortFields
-     * @return $this
-     */
-    public function setSortFields($sortFields): self
-    {
-        $this->sortFields = $sortFields;
-
-        return $this;
-    }
-
-    /**
-     * @return array
-     */
-    public function getFilterFields(): array
-    {
-        return $this->filterFields;
-    }
-
-    /**
-     * @param array $filterFields
-     * @return $this
-     */
-    public function setFilterFields($filterFields): self
-    {
-        $this->filterFields = $filterFields;
-
-        return $this;
-    }
-
-    /**
-     * @return array
-     */
-    public function getCopyFields(): array
-    {
-        return $this->copyFields;
-    }
-
-    /**
-     * @param array $copyField
-     * @return $this
-     */
-    public function setCopyFields($copyField): self
-    {
-        $this->copyFields = $copyField;
-
-        return $this;
-    }
-
-    /**
-     * @return string
-     */
-    public function getDefaultField(): string
-    {
-        return $this->defaultField;
-    }
-
-    /**
-     * @param string $defaultField
-     * @return $this
-     */
-    public function setDefaultField($defaultField): self
-    {
-        $this->defaultField = $defaultField;
-
-        return $this;
-    }
-
-    /**
-     * @param $sortField
-     * @return $this
-     */
-    public function addSortField($sortField): self
-    {
-        $this->addFulltextField($sortField);
-
-        $this->sortFields[] = $sortField;
-
-        $this->setSortFields(array_unique($this->getSortFields()));
-
-        return $this;
-    }
-
-    /**
-     * @param string $fulltextField
-     * @return $this
-     */
-    public function addFulltextField($fulltextField): self
-    {
-        $this->fulltextFields[] = $fulltextField;
-
-        return $this;
-    }
-
-    /**
-     * @param $field
-     * @param array $options
-     * @return $this
-     */
-    public function addFacetField($field, $options): self
-    {
-        $this->facetFields[$field] = $options;
-
-        if (!in_array($field, $this->getFilterFields(), true)) {
-            $this->addFilterField($field);
-        }
-
-        return $this;
-    }
-
-    /**
-     * @param $filterField
-     * @return $this
-     */
-    public function addFilterField($filterField): self
-    {
-        $this->filterFields[] = $filterField;
-
-        return $this;
-    }
-
-    /**
-     * @param string $field Name of the copyfield
-     * @param array $options Array of all fields that should be copied to this copyfield
-     * @return $this
-     */
-    public function addCopyField($field, $options): self
-    {
-        $this->copyFields[$field] = $options;
-
-        if (!in_array($field, $this->getFulltextFields(), true)) {
-            $this->addFulltextField($field);
-        }
-
-        return $this;
-    }
-
-    /**
-     * @return Client
-     */
-    public function getClient()
-    {
-        return $this->client;
-    }
-
-    /**
-     * @param Client $client
-     * @return BaseIndex
-     */
-    public function setClient($client): BaseIndex
-    {
-        $this->client = $client;
-
-        return $this;
     }
 }
---
+++ src/Traits/BaseIndexTrait.php
@@ -0,0 +1,192 @@
+<?php
+
+
+namespace Firesphere\SolrSearch\Traits;
+
+use Solarium\Core\Client\Client;
+
+trait BaseIndexTrait
+{
+    /**
+     * @return array
+     */
+    public function getFulltextFields(): array
+    {
+        return $this->fulltextFields;
+    }
+
+    /**
+     * @param array $fulltextFields
+     * @return $this
+     */
+    public function setFulltextFields($fulltextFields): self
+    {
+        $this->fulltextFields = $fulltextFields;
+
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getSortFields(): array
+    {
+        return $this->sortFields;
+    }
+
+    /**
+     * @param array $sortFields
+     * @return $this
+     */
+    public function setSortFields($sortFields): self
+    {
+        $this->sortFields = $sortFields;
+
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getFilterFields(): array
+    {
+        return $this->filterFields;
+    }
+
+    /**
+     * @param array $filterFields
+     * @return $this
+     */
+    public function setFilterFields($filterFields): self
+    {
+        $this->filterFields = $filterFields;
+
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getCopyFields(): array
+    {
+        return $this->copyFields;
+    }
+
+    /**
+     * @param array $copyField
+     * @return $this
+     */
+    public function setCopyFields($copyField): self
+    {
+        $this->copyFields = $copyField;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDefaultField(): string
+    {
+        return $this->defaultField;
+    }
+
+    /**
+     * @param string $defaultField
+     * @return $this
+     */
+    public function setDefaultField($defaultField): self
+    {
+        $this->defaultField = $defaultField;
+
+        return $this;
+    }
+
+    /**
+     * @param $sortField
+     * @return $this
+     */
+    public function addSortField($sortField): self
+    {
+        $this->addFulltextField($sortField);
+
+        $this->sortFields[] = $sortField;
+
+        $this->setSortFields(array_unique($this->getSortFields()));
+
+        return $this;
+    }
+
+    /**
+     * @param string $fulltextField
+     * @return $this
+     */
+    public function addFulltextField($fulltextField): self
+    {
+        $this->fulltextFields[] = $fulltextField;
+
+        return $this;
+    }
+
+    /**
+     * @param $field
+     * @param array $options
+     * @return $this
+     */
+    public function addFacetField($field, $options): self
+    {
+        $this->facetFields[$field] = $options;
+
+        if (!in_array($field, $this->getFilterFields(), true)) {
+            $this->addFilterField($field);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param $filterField
+     * @return $this
+     */
+    public function addFilterField($filterField): self
+    {
+        $this->filterFields[] = $filterField;
+
+        return $this;
+    }
+
+    /**
+     * @param string $field Name of the copyfield
+     * @param array $options Array of all fields that should be copied to this copyfield
+     * @return $this
+     */
+    public function addCopyField($field, $options): self
+    {
+        $this->copyFields[$field] = $options;
+
+        if (!in_array($field, $this->getFulltextFields(), true)) {
+            $this->addFulltextField($field);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Client
+     */
+    public function getClient()
+    {
+        return $this->client;
+    }
+
+    /**
+     * @param Client $client
+     * @return $this
+     */
+    public function setClient($client): self
+    {
+        $this->client = $client;
+
+        return $this;
+    }
+}
