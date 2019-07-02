c8e7973d0ee9b6b0b9ed7ae8fc3fd4085be10fe5
Simon Erkelens <github@casa-laguna.net>
2019-07-02 19:31:24 +1200

    init() needs to be at the bottom now
    - Move inside check for config.
    -- If no config, no need to check the classes
    -- Move requirement of init() to the bottom

--- src/Indexes/BaseIndex.php
+++ src/Indexes/BaseIndex.php
@@ -127,13 +127,13 @@ abstract class BaseIndex
         if (!self::config()->get($this->getIndexName())) {
             Deprecation::notice('5.0', 'Please set an index name');
 
+            // If the old init method is found, skip the config based init
+            if (count($this->getClasses())) {
+                Deprecation::notice('You are running init at the top of your method. The new API requires it to be at the bottom');
+            }
             return;
         }
 
-        // If the old init method is found, skip the config based init
-        if (count($this->getClasses())) {
-            return;
-        }
 
         $config = self::config()->get($this->getIndexName());
 
