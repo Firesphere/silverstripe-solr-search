6f9d6f753f9e632b6470b294c685233cb94fe7b8
Simon Erkelens <github@casa-laguna.net>
2019-07-06 15:11:23 +1200

    Yeah, nah

--- src/Services/SolrCoreService.php
+++ src/Services/SolrCoreService.php
@@ -16,7 +16,7 @@ use Solarium\QueryType\Server\CoreAdmin\Result\StatusResult;
 
 class SolrCoreService
 {
-    const ID_FIELD = SolrCoreService::ID_FIELD;
+    const ID_FIELD = '_documentid';
 
     use Configurable;
 
