800e2e405977811a8c4ccf80b205d9c4cdd1554b
Simon Erkelens <github@casa-laguna.net>
2019-07-07 13:06:47 +1200

    Minor code tweaks for minor improvements

--- src/Factories/DocumentFactory.php
+++ src/Factories/DocumentFactory.php
@@ -240,10 +240,10 @@ class DocumentFactory
      *
      * @param DataObject|array|SS_List $objects - The object to get the value from
      * @param array $field - The field definition to use
-     * @return array - The value of the field, or null if we couldn't look it up for some reason
+     * @return ArrayList|array|SS_List
      * @todo reduced the array_merge need to something more effective
      */
-    protected function getValueForField($objects, $field): array
+    protected function getValueForField($objects, $field)
     {
         $objects = is_iterable($objects) ? $objects : [$objects];
 
