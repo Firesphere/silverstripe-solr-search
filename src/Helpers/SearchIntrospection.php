406790f6c5b64bc2e65170ff35bf7350a0d3d91b
Simon Erkelens <github@casa-laguna.net>
2019-07-14 18:59:39 +1200

    Revert "A little abstraction"
    This reverts commit c3bdde1e809701d2cb334e6e58a3a1e75fc06b8c.

--- src/Helpers/SearchIntrospection.php
+++ src/Helpers/SearchIntrospection.php
@@ -290,7 +290,7 @@ class SearchIntrospection
      * @return array
      * @throws ReflectionException
      */
-    protected function getFieldOptions($field, array $sources, $fullfield, array $found)
+    protected function getFieldOptions($field, array $sources, $fullfield, array $found): array
     {
         foreach ($sources as $class => $fieldOptions) {
             if (is_int($class)) {
@@ -303,7 +303,36 @@ class SearchIntrospection
             $class = $this->getSourceName($class);
             $dataclasses = self::getHierarchy($class);
 
-            $found = $this->buildFieldForClass($field, $fullfield, $dataclasses, $class, $fieldOptions);
+            while (count($dataclasses)) {
+                $dataclass = array_shift($dataclasses);
+
+                $fields = DataObject::getSchema()->databaseFields($class);
+
+                [$type, $fieldOptions] = $this->getCallType($field, $fields, $fieldOptions, $dataclass);
+
+                if ($type) {
+                    // Don't search through child classes of a class we matched on. TODO: Should we?
+                    $dataclasses = array_diff($dataclasses, array_values(ClassInfo::subclassesFor($dataclass)));
+                    // Trim arguments off the type string
+                    if (preg_match('/^(\w+)\(/', $type, $match)) {
+                        $type = $match[1];
+                    }
+                    // Get the origin
+                    $origin = $fieldOptions['origin'] ?? $dataclass;
+
+                    $found["{$origin}_{$fullfield}"] = array(
+                        'name'         => "{$origin}_{$fullfield}",
+                        'field'        => $field,
+                        'fullfield'    => $fullfield,
+                        'origin'       => $origin,
+                        'class'        => $dataclass,
+                        'lookup_chain' => $fieldOptions['lookup_chain'],
+                        'type'         => $type,
+                        'multi_valued' => isset($fieldOptions['multi_valued']) ? true : false,
+                    );
+                }
+            }
+            $this->found[$class . '_' . $fullfield] = $found;
         }
 
 
