5f8f1dc8be5b2be71de7f9df44be70a9ec0d4a2d
Simon Erkelens <github@casa-laguna.net>
2019-07-14 14:34:02 +1200

    The post really does not like to be tested

--- tests/unit/ConfigStoreTest.php
+++ tests/unit/ConfigStoreTest.php
@@ -53,36 +53,4 @@ class ConfigStoreTest extends SapphireTest
 
         $this->assertEquals('/', $store->getPath());
     }
-
-    /**
-     * In reality, it _should_ return a 201
-     * But for tests, we don't really care about the return, we care about the execution
-     */
-    public function testPostUploadString()
-    {
-        // Just post to whereever is convenient, in this case, home.
-        $store = new PostConfigStore(['uri' => 'http://localhost', 'path' => '']);
-
-        // We only care that it _executes_ the post, not what's returned
-        $response = $store->uploadString('', 'test.txt', 'this, is, a, test');
-
-        $this->assertInstanceOf(ResponseInterface::class, $response);
-        $this->assertEquals(200, $response->getStatusCode());
-    }
-
-    /**
-     * In reality, it _should_ return a 201
-     * But for tests, we don't really care about the return, we care about the execution
-     */
-    public function testPostUploadFile()
-    {
-        // Just post to whereever is convenient, in this case, home.
-        $store = new PostConfigStore(['uri' => 'http://localhost', 'path' => '']);
-
-        // We only care that it _executes_ the post, not what's returned
-        $response = $store->uploadFile('', Director::baseFolder() . '/app/_config/search.yml');
-
-        $this->assertInstanceOf(ResponseInterface::class, $response);
-        $this->assertEquals(200, $response->getStatusCode());
-    }
 }
