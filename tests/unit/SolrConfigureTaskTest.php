446b195e95a82e1263d7d20294f52e00f0cd3536
Simon Erkelens <github@casa-laguna.net>
2019-07-14 15:04:15 +1200

    Update test coverage a bit more

---
+++ tests/unit/SolrConfigureTaskTest.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace Firesphere\SolrSearch\Tests;
+
+
+use Firesphere\SolrSearch\Tasks\SolrConfigureTask;
+use Psr\Log\LoggerInterface;
+use SilverStripe\Dev\SapphireTest;
+
+class SolrConfigureTaskTest extends SapphireTest
+{
+
+    public function testGetLogger()
+    {
+        $task = new SolrConfigureTask();
+
+        $this->assertInstanceOf(LoggerInterface::class, $task->getLogger());
+    }
+}
