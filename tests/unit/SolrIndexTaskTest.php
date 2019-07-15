202950301b62845353f99e0264d9dd4dd6f069cb
Simon Erkelens <github@casa-laguna.net>
2019-07-15 13:33:21 +1200

    Fix count

--- tests/unit/SolrIndexTaskTest.php
+++ tests/unit/SolrIndexTaskTest.php
@@ -24,7 +24,7 @@ class SolrIndexTaskTest extends SapphireTest
 
         $result = $task->run($request);
 
-        $this->assertEquals(0, $result);
+        $this->assertEquals(1, $result);
 
         $getVars = [
             'group' => 0,
