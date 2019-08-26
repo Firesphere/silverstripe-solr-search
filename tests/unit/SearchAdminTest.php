1631690d94858a4b840a33c539b4031edc720514
Simon Erkelens <github@casa-laguna.net>
2019-08-26 14:19:09 +1200

    Make sure the requirements are loaded

---
+++ tests/unit/SearchAdminTest.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace Firesphere\SolrSearch\Tests;
+
+
+use Firesphere\SolrSearch\Admins\SearchAdmin;
+use SilverStripe\Dev\Debug;
+use SilverStripe\Dev\SapphireTest;
+use SilverStripe\View\Requirements;
+
+class SearchAdminTest extends SapphireTest
+{
+    public function testInit()
+    {
+        $admin = new SearchAdmin();
+        $admin->init();
+        $this->arrayHasKey('solr-search/client/dist/main.js', Requirements::backend()->getCSS());
+    }
+}
