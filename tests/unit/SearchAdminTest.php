26079dfedd07cb1ea60fa0fa60d3691c9fcc436c
Simon Erkelens <github@casa-laguna.net>
2019-08-26 14:28:07 +1200

    It needs a session for some reason

--- tests/unit/SearchAdminTest.php
+++ tests/unit/SearchAdminTest.php
@@ -5,6 +5,8 @@ namespace Firesphere\SolrSearch\Tests;
 
 
 use Firesphere\SolrSearch\Admins\SearchAdmin;
+use SilverStripe\Control\Controller;
+use SilverStripe\Control\Session;
 use SilverStripe\Dev\SapphireTest;
 use SilverStripe\View\Requirements;
 
@@ -13,7 +15,9 @@ class SearchAdminTest extends SapphireTest
     public function testInit()
     {
         $admin = new SearchAdmin();
+        $session = new Session(['hello' => 'world']);
         $admin->init();
+        Controller::curr()->getRequest()->setSession($session);
         $this->arrayHasKey('solr-search/client/dist/main.js', Requirements::backend()->getCSS());
     }
 }
