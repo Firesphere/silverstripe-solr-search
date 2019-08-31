01844903d3d4932c15fa04ca561bf70ff4f6f3ca
Simon Erkelens <github@casa-laguna.net>
2019-09-01 10:13:50 +1200

    Add permission provider test

--- tests/unit/SolrLogTest.php
+++ tests/unit/SolrLogTest.php
@@ -32,6 +32,30 @@ class SolrLogTest extends SapphireTest
         $this->assertTrue($this->log->canView($admin));
     }
 
+    public function testProvidePermissions()
+    {
+        $expected = [
+            'DELETE_LOG' => [
+                'name'     => _t(self::class . '.PERMISSION_DELETE_DESCRIPTION', 'Delete Solr logs'),
+                'category' => _t('Permissions.LOGS_CATEGORIES', 'Solr logs permissions'),
+                'help'     => _t(
+                    self::class . '.PERMISSION_DELETE_HELP',
+                    'Permission required to delete existing Solr logs.'
+                )
+            ],
+            'VIEW_LOG'   => [
+                'name'     => _t(self::class . '.PERMISSION_VIEW_DESCRIPTION', 'View Solr logs'),
+                'category' => _t('Permissions.LOGS_CATEGORIES', 'Solr logs permissions'),
+                'help'     => _t(
+                    self::class . '.PERMISSION_VIEW_HELP',
+                    'Permission required to view existing Solr logs.'
+                )
+            ],
+        ];
+
+        $this->assertEquals($expected, $this->log->providePermissions());
+    }
+
     public function testGetLastErrorLine()
     {
         $error = SolrLog::create(['Message' => "Test\nNew\nLine", 'Level' => 'ERROR']);
