5876750f97c9d73d074721f381ebc8dec4d4919e
Simon <simon@casa-laguna.net>
2019-09-02 22:41:57 +1200

    Add an empty index for testing purposes
    To be used to cover continue statement. Might need some adjusting later

---
+++ tests/mocks/TestIndex3.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace Firesphere\SolrSearch\Tests;
+
+
+use Firesphere\SolrSearch\Indexes\BaseIndex;
+use SilverStripe\Dev\TestOnly;
+
+class TestIndex3 extends BaseIndex implements TestOnly
+{
+
+    public function init()
+    {
+        return;
+    }
+
+    public function getIndexName()
+    {
+        return 'TestIndex-3';
+    }
+}
