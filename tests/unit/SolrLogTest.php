b082c0f43b8bf6f578b57c2eb0c365602a2e2b1f
Simon <simon@casa-laguna.net>
2019-09-04 16:03:31 +1200

    I was testing the wrong thing.

--- tests/unit/SolrLogTest.php
+++ tests/unit/SolrLogTest.php
@@ -24,35 +24,11 @@ class SolrLogTest extends SapphireTest
     {
         $this->assertFalse($this->log->canCreate());
         $this->assertFalse($this->log->canEdit());
-        $this->assertFalse($this->log->canDelete());
         /** @var DefaultAdminService $admin */
         $admin = singleton(DefaultAdminService::class);
         $admin = $admin->findOrCreateDefaultAdmin();
         $this->assertTrue($this->log->canView($admin));
-    }
-
-    public function testProvidePermissions()
-    {
-        $expected = [
-            'DELETE_LOG' => [
-                'name'     => _t(self::class . '.PERMISSION_DELETE_DESCRIPTION', 'Delete Solr logs'),
-                'category' => _t('Permissions.LOGS_CATEGORIES', 'Solr logs permissions'),
-                'help'     => _t(
-                    self::class . '.PERMISSION_DELETE_HELP',
-                    'Permission required to delete existing Solr logs.'
-                )
-            ],
-            'VIEW_LOG'   => [
-                'name'     => _t(self::class . '.PERMISSION_VIEW_DESCRIPTION', 'View Solr logs'),
-                'category' => _t('Permissions.LOGS_CATEGORIES', 'Solr logs permissions'),
-                'help'     => _t(
-                    self::class . '.PERMISSION_VIEW_HELP',
-                    'Permission required to view existing Solr logs.'
-                )
-            ],
-        ];
-
-        $this->assertEquals($expected, $this->log->providePermissions());
+        $this->assertTrue($this->log->canDelete($admin));
     }
 
     public function testGetLastErrorLine()
