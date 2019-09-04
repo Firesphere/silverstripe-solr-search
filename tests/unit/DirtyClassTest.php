62a3aa397ed6d8ed368e5c7946c84008c64d51cd
Simon <simon@casa-laguna.net>
2019-09-04 15:49:12 +1200

    Fixup tests

--- tests/unit/DirtyClassTest.php
+++ tests/unit/DirtyClassTest.php
@@ -5,6 +5,7 @@ namespace Firesphere\SolrSearch\Tests;
 
 
 use Firesphere\SolrSearch\Models\DirtyClass;
+use SilverStripe\CMS\Model\SiteTree;
 use SilverStripe\Dev\SapphireTest;
 
 class DirtyClassTest extends SapphireTest
@@ -17,5 +18,14 @@ class DirtyClassTest extends SapphireTest
         $this->assertFalse($dirtyClass->canView(null, []));
         $this->assertFalse($dirtyClass->canEdit(null));
         $this->assertFalse($dirtyClass->canDelete(null));
+    }
+
+    public function testGetCMSFields()
+    {
+        $class = DirtyClass::create(['Class' => SiteTree::class]);
+        $fields = $class->getCMSFields();
+
+        $this->assertNotNull($fields->dataFieldByName('Class'));
+        $this->assertNotNull($fields->dataFieldByName('IDs'));
     }
 }
