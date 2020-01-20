4faee1178829234b38149522c2fa8aeed2a59a93
Simon 'Firesphere' Erkelens <github@casa-laguna.net>
2020-01-21 10:50:50 +1300

    Update the suggest to have a function

--- client/proxy/autosuggest.php
+++ client/proxy/autosuggest.php
@@ -9,15 +9,24 @@
  * @author Simon `Firesphere` Erkelens; Marco `Sheepy` Hermo
  * @copyright Copyright (c) 2018 - now() Firesphere & Sheepy
  */
+
 use GuzzleHttp\Client;
 
-if (file_exists($_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php')) {
-    require $_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php';
+/**
+ * Get a suggestion from Solr
+ */
+function getSuggestion()
+{
+    if (file_exists($_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php')) {
+        require $_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php';
+    }
+
+    $request = filter_input(INPUT_GET, 'query', FILTER_SANITIZE_STRING);
+
+    $client = new Client();
+    $suggestions = $client->request('GET', 'http://localhost:8983/solr/My-Solr-Core/suggest?q=' . $request);
+    header('Content-Type: application/json');
+    echo($suggestions->getBody());
 }
 
-$request = filter_input(INPUT_GET, 'query', FILTER_SANITIZE_STRING);
-
-$client = new Client();
-$suggestions = $client->request('GET', 'http://localhost:8983/solr/My-Solr-Core/suggest?q=' . $request);
-header('Content-Type: application/json');
-echo($suggestions->getBody());
+getSuggestion();
