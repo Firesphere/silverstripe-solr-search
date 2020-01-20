31fdf8052f3b1431012d114002a17a5c2a33f05d
Simon 'Firesphere' Erkelens <github@casa-laguna.net>
2020-01-20 13:04:08 +1300

    Try to use the doc XML file

--- _config.php
+++ _config.php
@@ -1,13 +1,16 @@
 <?php
+/**
+ * Basic SilverStripe config support file
+ */
 
 /**
+ * Global method, to make it easier to address and less convoluted.
+ *
  * Get the short name of a field, e.g. App\Project\Page_Title will become Page_Title
  * Used to remove unwanted backslashes from Solr cores.
  *
- * Global method, to make it easier to address and less convoluted.
- *
- * @param string $name
- * @return string mixed
+ * @param string $name Name to be shortened
+ * @return string Short name
  * @package \
  */
 function getShortFieldName($name)
